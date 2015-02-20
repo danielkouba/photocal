2baf0feb3a3b94f849211eb47ae5c39dcc45e727
danielkouba <koubaphotog@gmail.com>
2015-02-20 15:10:28 -0800

    initial JSON files added

---
+++ jqueryVersion.js
@@ -0,0 +1,15 @@
+(function($) {
+  var insertPeople = function (people) {
+    var $people = $('#people');
+    for (var i = 0, ii = people.length; i < ii; i++) {
+      var person = people[i];
+      var $element = $('<li></li>').addClass(person.profession);
+      $element.append('<span class="name">' + person.name + '</span>');
+      $element.append('<span class="url">' + '<a href="http://' + person.url + '"> go there </a>' + '</span>');
+      $element.append('<span class="venue">' + person.venue + '</span>');
+      $people.append($element);
+    }
+  };
+
+  $.getJSON('exhibits.json', insertPeople);
+})(jQuery); 
---
+++ min/jqueryVersion-min.js
@@ -0,0 +1,1 @@
+!function($){var a=function(a){for(var n=$("#people"),e=0,p=a.length;p>e;e++){var s=a[e],l=$("<li></li>").addClass(s.profession);l.append('<span class="name">'+s.name+"</span>"),l.append('<span class="url"><a href="http://'+s.url+'"> go there </a></span>'),l.append('<span class="venue">'+s.venue+"</span>"),n.append(l)}};$.getJSON("exhibits.json",a)}(jQuery);
