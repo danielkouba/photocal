7a144132ac1e67adc34f7ab9b5cac5dcb9a8c106
danielkouba <koubaphotog@gmail.com>
2015-02-22 21:44:29 -0800

    JSON implemented,everything works

--- scraper.rb
+++ scraper.rb
@@ -9,7 +9,7 @@ def urlList
 	##The list of URLs we will be running through
 	$url_array = [[
 		proc{ pullHtml($url_array[0][1]).css("a.url").map{ |a| 
-			[a.text , "http://www.sfmoma.org" + a['href']]}},
+			[a.text , "www.sfmoma.org" + a['href']]}},
 	"http://www.sfmoma.org/exhib_events/calendar?start=20150216&end=20160216&range=day&change-year=2015&category=exhibition"]]
 end
 
@@ -23,6 +23,7 @@ end
 def scrape
 	x = 0
 	array = []
+	$pageHash = []
 	urlList
 	##runs pullHTML
 	##uses $url_array but can change to urlList later
@@ -30,12 +31,11 @@ def scrape
 	link_array = $url_array[0][0].call
 	while x <= (link_array.length - 1)
 		##something like this
-		puts "name: "  + link_array[x][0] + "\n " + "url: " + link_array[x][1] + "\n"
+		$pageHash << {"name" => "#{link_array[x][0]}", "url" => "#{link_array[x][1]}"}
 		x +=1
 	end
 end
 
-scrape
 
 
 
@@ -46,14 +46,20 @@ scrape
 #
 def readFile
 	file = File.open("exhibits.json", "r+")
-	rb_hash = JSON.parse(file.read)
-	rb_hash[0] << { "name" => "Nonsense"}
-	rb_json = JSON.pretty_generate(rb_hash)
+	$rb_hash = JSON.parse(file.read)
+	$pageHash.each do |h|
+		$rb_hash << h
+	end
+	$rb_json = JSON.pretty_generate($rb_hash)
 	file.close
+	writeFile
 end
 
 def writeFile
 	file = File.open("exhibits.json", "w")
-	file.write(rb_json)
+	file.write($rb_json)
 	file.close
 end
+
+scrape
+readFile
